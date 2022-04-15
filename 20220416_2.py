e6958816009ef3aa61a21149f30b51eff75236c2
ppk150 <67466148+ppk150@users.noreply.github.com>
2022-04-16 07:43:33 +0900

    1차원 배열문제_2
    9개의 서로 다른 자연수가 주어질 때, 이들 중 최댓값을 찾고 그 최댓값이 몇 번째 수인지를 구하는 프로그램을 작성하시오.
    
    예를 들어, 서로 다른 9개의 자연수
    
    3, 29, 38, 12, 57, 74, 40, 85, 61
    
    이 주어지면, 이들 중 최댓값은 85이고, 이 값은 8번째 수이다.
    
    입력
    첫째 줄부터 아홉 번째 줄까지 한 줄에 하나의 자연수가 주어진다. 주어지는 자연수는 100 보다 작다.
    
    출력
    첫째 줄에 최댓값을 출력하고, 둘째 줄에 최댓값이 몇 번째 수인지를 출력한다.

---
+++ 20220416_2.py
@@ -0,0 +1,21 @@
+
+k = range(9)
+
+NUM = []
+
+for i in k:
+    N =input()
+    intNum = int(N)
+    NUM.append(intNum)
+    arrayNUM = NUM
+    arrayNUM.sort()
+
+
+
+for i in k :
+    if arrayNUM[8] == NUM[i]:
+
+        arrayNUMBER = i
+
+print(arrayNUM[8])
+print(arrayNUMBER)
