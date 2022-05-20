46dbbcfda618b55bcf93e1f3c51262a28ab90126
ppk150 <67466148+ppk150@users.noreply.github.com>
2022-05-20 20:16:21 +0900

    기본수학_벌집문제
    문제
    
    https://www.acmicpc.net/JudgeOnline/upload/201009/3(2).png
    
    위의 그림과 같이 육각형으로 이루어진 벌집이 있다. 그림에서 보는 바와 같이 중앙의 방 1부터 시작해서 이웃하는 방에 돌아가면서 1씩 증가하는 번호를 주소로 매길 수 있다. 숫자 N이 주어졌을 때, 벌집의 중앙 1에서 N번 방까지 최소 개수의 방을 지나서 갈 때 몇 개의 방을 지나가는지(시작과 끝을 포함하여)를 계산하는 프로그램을 작성하시오. 예를 들면, 13까지는 3개, 58까지는 5개를 지난다.
    
    입력
    첫째 줄에 N(1 ≤ N ≤ 1,000,000,000)이 주어진다.
    
    출력
    입력으로 주어진 방까지 최소 개수의 방을 지나서 갈 때 몇 개의 방을 지나는지 출력한다.

---
+++ 20220520_1.py
@@ -0,0 +1,21 @@
+
+
+N = input()
+
+Num = int(N)
+
+count = 1
+
+Total = 1
+
+end = 1
+
+
+while end :
+
+    Total = Total + (count *6 -6)
+    if Total >= Num:
+        print(count)
+        end = 0
+
+    count = count +1
