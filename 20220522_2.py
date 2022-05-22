f0a7e80a89625103f9a8682fc0c10d01476b1ac2
ppk150 <67466148+ppk150@users.noreply.github.com>
2022-05-22 20:32:10 +0900

    기본수학_달팽이는 올라가고 싶다_2(수식버젼)
    문제
    땅 위에 달팽이가 있다. 이 달팽이는 높이가 V미터인 나무 막대를 올라갈 것이다.
    
    달팽이는 낮에 A미터 올라갈 수 있다. 하지만, 밤에 잠을 자는 동안 B미터 미끄러진다. 또, 정상에 올라간 후에는 미끄러지지 않는다.
    
    달팽이가 나무 막대를 모두 올라가려면, 며칠이 걸리는지 구하는 프로그램을 작성하시오.
    
    입력
    첫째 줄에 세 정수 A, B, V가 공백으로 구분되어서 주어진다. (1 ≤ B < A ≤ V ≤ 1,000,000,000)
    
    출력
    첫째 줄에 달팽이가 나무 막대를 모두 올라가는데 며칠이 걸리는지 출력한다.

---
+++ 20220522_2.py
@@ -0,0 +1,22 @@
+
+A = input()
+
+Anum = int(A)
+
+B = input()
+
+Bnum = int(B)
+
+V =input()
+
+Vnum = int(V)
+
+climb = 0
+
+#count = 0
+
+end = 1
+
+climb = (Vnum - Bnum-1) / (Anum-Bnum) +1
+
+print (climb)
